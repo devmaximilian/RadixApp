bdbeab4bac45fb04ebf50cb5c46c3a564f6af6a0
Maximilian Wendel <maximilian.wendel@mwtdev.se>
2021-03-21 16:19:26 +0100

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Maximilian Wendel on 2021-03-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/RadixAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadixAppApp.swift
+//  Shared
+//
+//  Created by Maximilian Wendel on 2021-03-21.
+//
+
+import SwiftUI
+
+@main
+struct RadixAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
